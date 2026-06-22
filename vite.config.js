14b3cfea5415f4ec0eccdcc4dc3be465387b64b5
ABDR-Again <a.b.d.r1912003mltcqa.b.d.r@gmail.com>
2026-06-22 23:46:57 +0500

    fixed lighthouse performance warnings

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,16 @@ export default defineConfig({
   css: {
     transformer: 'lightningcss',
   },
+  plugins: [
+    {
+      name: 'defer-css',
+      enforce: 'post',
+      transformIndexHtml(html) {
+        return html.replace(
+          /<link rel="stylesheet" crossorigin href="([^"]+)">/g,
+          '<link rel="preload" href="$1" as="style" onload="this.onload=null;this.rel=\'stylesheet\'"><noscript><link rel="stylesheet" href="$1"></noscript>'
+        );
+      }
+    }
+  ]
 });
